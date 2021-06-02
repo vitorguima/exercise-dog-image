c33472ab07ff25985b4c75a120ef589b15fcebe8
Vitor <vitorguima@gmail.com>
2021-06-02 20:08:14 -0300

    homework ate req 2

--- src/App.js
+++ src/App.js
@@ -1,30 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>
-            src/App.js
-          </code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      image: '',
+      loading: true,
+      count: 0,
+    }
+
+    this.fetchDogs = this.fetchDogs.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
+  }
+
+  fetchDogs() {
+    const Url = 'https://dog.ceo/api/breeds/image/random'
+    this.setState(
+      { loading: true },
+      () => {fetch(Url)
+      .then((response) => response.json())
+      .then(({ message }) => this.setState((state) => ({
+        image: message,
+        loading: false,
+        count:  state.count + 1,
+      })))
+      .then(() => localStorage.setItem(this.state.count, this.state.image))
+      }
+    )
+  }
+
+  shouldComponentUpdate(_nextProps, { image }) {
+    return !image.includes('terrier');
+  }
+
+  handleSearch() {
+    this.fetchDogs();
+    window.alert(this.state.image.split('/')[4].split('-').join(' '));
+  }
+
+  componentDidMount() {
+    this.fetchDogs();
+  }
+
+  render() {
+    const { image, loading } = this.state;
+    
+    return (
+      <div className="App">
+       <div className="image-wrapper">
+      { loading === true ? "Loading..." : <img src={image} /> }
+      </div> 
+      <button onClick={this.handleSearch}>Search</button>
+      </div>
+    );
+  }
 }
 
 export default App;
